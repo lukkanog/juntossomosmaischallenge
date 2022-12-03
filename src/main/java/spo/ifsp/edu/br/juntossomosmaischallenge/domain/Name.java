4eec980d9952f12cb986f49c3c6bcb044b014308
nightFirefox <gabriel.zacari@gmail.com>
2022-12-03 00:30:45 -0300

    refactor: change constructor order in Name class

--- src/main/java/spo/ifsp/edu/br/juntossomosmaischallenge/domain/Name.java
+++ src/main/java/spo/ifsp/edu/br/juntossomosmaischallenge/domain/Name.java
@@ -1,15 +1,15 @@
 package spo.ifsp.edu.br.juntossomosmaischallenge.domain;
 
 public class Name {
-    private String title;
-    private String first;
-    private String last;
-
     public Name(String title, String first, String last) {
         this.title = title;
         this.first = first;
         this.last = last;
     }
+    
+    private String title;
+    private String first;
+    private String last;
 
     @Override
     public String toString() {
